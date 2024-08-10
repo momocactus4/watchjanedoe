18fbc10f8e81556a0ea8a58eb7e73bc322e10061
momo <mgp75946@gmail.com>
2024-08-10 20:56:14 -0300

    i just changed text for something more dramatic

--- janedoe.py
+++ janedoe.py
@@ -2,15 +2,15 @@ import webbrowser
 from tkinter import *
 
 window = Tk()
-window.title("DdP Program")
+window.title("Jane Doe")
 window.geometry("300x100")
 
 def onClick(x):
     webbrowser.open(x, new=1)
-label = Label(text="jane doe")
+label = Label(text="and im asking whyyyy lord?")
 label.pack()
 
-labelOne = Label(text="Just john and me, forever eternally jane doe.")
+labelOne = Label(text="if this is how i dieeeeeee lord???.")
 labelOne.pack()
 
 url = "https://www.youtube.com/watch?v=-qUQ-YSW6Do"
